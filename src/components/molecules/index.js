e748787d798ed96a47531c7ff26a203b60d20c48
Hairul <98758226+HairulDev@users.noreply.github.com>
2023-04-18 22:39:14 +0700

    Update index.js

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,11 +1,10 @@
 import CardItem from './CardItem';
-import Categories from './Categories';
 import Pagination from './Pagination';
 import UserForm from './UserForm';
 import Logout from './Logout';
 import NotFound from './NotFound';
 
 export {
-    CardItem, Categories, Pagination,
+    CardItem, Pagination,
     UserForm, Logout, NotFound,
 };
