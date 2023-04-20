071267cd613e3d9f15a315e47b665157a25dc0cf
Hairul <98758226+HairulDev@users.noreply.github.com>
2023-04-20 09:17:16 +0700

    add api key auth

--- src/configs/axios/index.js
+++ src/configs/axios/index.js
@@ -3,9 +3,20 @@ import env from "configs/vars";
 
 const API = axios.create({ baseURL: env.reactAppHost });
 const user = JSON.parse(localStorage.getItem('profile'));
+
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  Authorization: `Bearer ${user?.token ?? ''}`,
+  'Content-Type': 'application/json',
+  'x-api-key': env.apiKey,
+};
+
 API.interceptors.request.use((req) => {
-  req.headers.authorization = `Bearer ${user.token}`;
+  console.log("interceptornye===>>", req);
+  req.headers = headers;
   return req;
 });
+
 
 export default API;
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,7 +1,24 @@
 import { AUTH, } from "../../constants/actionTypes";
 import axios from "axios";
 import env from "configs/vars";
+
 const API = axios.create({ baseURL: env.reactAppHost });
+const user = JSON.parse(localStorage.getItem('profile'));
+
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  Authorization: `Bearer ${user?.token ?? ''}`,
+  'Content-Type': 'application/json',
+  'x-api-key': env.apiKey,
+};
+
+API.interceptors.request.use((req) => {
+  console.log("interceptornye===>>", req);
+  req.headers = headers;
+  return req;
+});
+
 
 export const signin = (formData, successCB, failedCB) => async (dispatch) => {
   API.post("/v1/auth/signin", formData)
